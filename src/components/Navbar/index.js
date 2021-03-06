c9ed1d0848a272b083ff443a595f30a2392d8941
Carmen Hui <karmenzkarmenz@gmail.com>
2021-03-06 18:06:55 -0500

    modified navbar, footer
    added projects

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function Navbar(){
     return (
         <nav className="grey darken-2">
-            <div className="nav-wrapper">
-            <a href="/" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <div class="nav-wrapper">
+            <a href="/"><img src="https://i.postimg.cc/tCXtcXGJ/logo.png" className="logo" alt="logo"></img></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
                 <li><a href="collapsible.html">JavaScript</a></li>
